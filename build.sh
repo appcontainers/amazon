b512bbd58f16a0b8112d31cfc256f76378d45bd5
Richard Nason <rnason@atlantean.io>
2017-05-16 09:04:39 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,21 +1,29 @@
+# This build script will create the docker images for the Amazon Linux Base Image
+# 2 Images will be created, one bare, and the other including Ansible
+# Both images will contain the AWS CLI
+
+# CD into the Main Project directory before launching this script
+
+# Base Container Image
 cd base
 docker build -t build/amazon .
 docker run -it -d --name amazon build/amazon /bin/bash
 docker export amazon | docker import - appcontainers/amazon:latest
-docker tag appcontainers/amazon:latest appcontainers/amazon:2017.03
+docker tag "appcontainers/amazon:latest" "appcontainers/amazon:2017.03"
 docker kill amazon; docker rm amazon
-docker push appcontainers/amazon:latest
-docker push appcontainers/amazon:2017.03
+docker push "appcontainers/amazon:latest"
+docker push "appcontainers/amazon:2017.03"
 docker rmi build/amazon
-docker rmi appcontainers/amazon:2017.03
+docker rmi "appcontainers/amazon:2017.03"
 
+# Base Container Image with Ansible
 cd ../ansible
 docker build -t build/amazon .
 docker run -it -d --name amazon build/amazon /bin/bash
 docker export amazon | docker import - appcontainers/amazon:ansible
-docker tag appcontainers/amazon:latest appcontainers/amazon:ansible-2017.03
+docker tag "appcontainers/amazon:latest" "appcontainers/amazon:ansible-2017.03"
 docker kill amazon; docker rm amazon
-docker push appcontainers/amazon:ansible
-docker push appcontainers/amazon:ansible-2017.03
+docker push "appcontainers/amazon:ansible"
+docker push "appcontainers/amazon:ansible-2017.03"
 docker rmi build/amazon
-docker rmi appcontainers/amazon:ansible-2017.03
+docker rmi "appcontainers/amazon:ansible-2017.03"
